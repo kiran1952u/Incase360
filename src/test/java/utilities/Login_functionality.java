a75f18630b6e9bce19dd669f34e82ea933c5da2b
Kiran_Presolv360 <kirannikam211@gmail.com>
2024-06-20 17:20:51 +0530

    Bulk send

---
+++ src/test/java/utilities/Login_functionality.java
@@ -0,0 +1,33 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Login_functionality {
+
+    public void Login(WebDriver driver) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver.get("https://test.incase360.com/login");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        driver.findElement(By.name("userName")).sendKeys("admin@incase360.com");
+        driver.findElement(By.name("userPassword")).sendKeys("1WbFG0Z84@");
+        Thread.sleep(3000);
+        WebElement e = driver.findElement(By.id("captchaanswer"));
+        System.out.println("This is the value: " + e.getAttribute("innerHTML"));
+        Thread.sleep(1000);
+        driver.findElement(By.id("captcha")).sendKeys(e.getAttribute("innerHTML"));
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/section/main/div/div/div/div/div/div/div/form/div[4]/div[2]/button")).click();
+    }
+
+
+}
+
+
