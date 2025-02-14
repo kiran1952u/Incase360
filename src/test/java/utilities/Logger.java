c071570f525eadd8fc54bb11d0992d6dd63d4e73
Kiran_Presolv360 <kirannikam211@gmail.com>
2025-02-14 16:33:32 +0530

    untilites changes

--- src/test/java/utilities/Logger.java
+++ src/test/java/utilities/Logger.java
@@ -7,9 +7,9 @@ public class Logger {
     private static final org.apache.log4j.Logger logger = LogManager.getLogger(LoggingExample.class);
 
     public static void main(String[] args) {
-        logger.debug("This is a debug message");
+        logger.debug("This sdfsd a debug message");
         logger.info("This is an info message");
-        logger.warn("This is a warning message");
+        logger.warn("This is asdfsd warning message");
         logger.error("This is an error message");
         logger.fatal("This is a fatal message");
     }
